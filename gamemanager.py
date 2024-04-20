81bba1ac35b19824e78fa9b5cd887ca6136c7cb4
unknown <kadron@post.bgu.ac.il>
2024-04-20 19:36:13 +0300

    server done, add logger and connectionhandler

--- gamemanager.py
+++ gamemanager.py
@@ -1,29 +1,39 @@
 
 pokemon_statements = [
-    ("Pikachu evolves into Raichu by using a Thunder Stone.", "True"),
-    ("The Pokémon Bulbasaur is a Grass/Poison-type.", "True"),
-    ("Charmander is a Water-type Pokémon.", "False"),
-    ("Mew is a Mythical Pokémon.", "True"),
+    ("Pikachu evolves into Raichu by using a Thunder Stone.", True),
+    ("The Pokémon Bulbasaur is a Grass/Poison-type.", True),
+    ("Charmander is a Water-type Pokémon.", False),
+    ("Mew is a Mythical Pokémon.", True),
     ("Ash Ketchum is the main protagonist in the Pokémon anime series.", "True"),
-    ("The move 'Splash' deals damage to the opponent.", "False"),
-    ("Dragonite is known as the Dragon Pokémon.", "True"),
-    ("Jigglypuff's song can put its audience to sleep.", "True"),
-    ("Meowth is a member of Team Rocket.", "True"),
-    ("Gyarados evolves from Magikarp.", "True"),
-    ("Eevee can evolve into seven different Pokémon.", "False"),
-    ("Charizard is a Fire/Flying-type Pokémon.", "True"),
-    ("Pikachu's pre-evolution is Pichu.", "False"),
-    ("Mewtwo is the result of genetic experiments.", "True"),
-    ("Snorlax is known for its high speed.", "False"),
-    ("The Pokémon Ditto can transform into any other Pokémon.", "True"),
-    ("Gengar is weak against Psychic-type moves.", "True"),
-    ("The Legendary Pokémon Articuno is a Fire-type.", "False"),
-    ("Machop evolves into Machoke, which then evolves into Machamp.", "True"),
-    ("The Pokémon Eevee has only one evolution form.", "False")
+    ("The move 'Splash' deals damage to the opponent.", False),
+    ("Dragonite is known as the Dragon Pokémon.", True),
+    ("Jigglypuff's song can put its audience to sleep.", True),
+    ("Meowth is a member of Team Rocket.", True),
+    ("Gyarados evolves from Magikarp.", True),
+    ("Eevee can evolve into seven different Pokémon.", False),
+    ("Charizard is a Fire/Flying-type Pokémon.", True),
+    ("Pikachu's pre-evolution is Pichu.", False),
+    ("Mewtwo is the result of genetic experiments.", True),
+    ("Snorlax is known for its high speed.", False),
+    ("The Pokémon Ditto can transform into any other Pokémon.", True),
+    ("Gengar is weak against Psychic-type moves.", True),
+    ("The Legendary Pokémon Articuno is a Fire-type.", False),
+    ("Machop evolves into Machoke, which then evolves into Machamp.", True),
+    ("The Pokémon Eevee has only one evolution form.", False)
 ]
 
 import random
-import functools
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 class Player:
     '''Internal GameManager class to help keep track of each player seperately.'''
@@ -33,7 +43,6 @@ class Player:
         self._name = name
         self._disqualified = False
         self._score = 0
-        self._is_active = True
         self._has_answered_this_round = False
 
     
@@ -46,9 +55,9 @@ class Player:
     def get_player_num(self):
         return self._player_num
     
-    def is_active(self):
-        return self._is_active
-    
+    def get_score(self):
+        return self._score
+       
     def is_disqualified(self):
         return self._disqualified
     
@@ -66,30 +75,33 @@ class Player:
     def undisqualify(self):
         self._disqualified = False
 
-    def deactivate(self):
-        self._is_active = False
-
     def prep_to_next_round(self):
         self._has_answered_this_round = False
 
     
 
-    
-    
-
-
 class GameManager:
     def __init__(self):
         self._players = dict()
+        self._active_players_ids = list()
         self._round = 1
         self._player_num = 1
-        self._statements = pokemon_statements
+        self._statements = pokemon_statements.copy()
+        self._total_rounds = len(self._statements)
         self._this_round_statement = None
+        self._game_active = True
     
+    def game_active(self):
+        return self._game_active
+
     def add_player(self, id, name):
         self._players[id] = Player(id, name, self._player_num)
+        self._active_players_ids.append(id)
         self._player_num += 1
     
+    def deactivate_player(self, id):
+        self._active_players_ids.remove(id)
+
     def round_init(self):
         '''The function check for remaining player and returns pre-round string of the remaining players:
 
@@ -102,12 +114,12 @@ class GameManager:
             ====\n
             "
         '''
-        ret = "Round " + str(self._round) +":\n"
-        for player in self._players.values():
-            if player.is_active():
-                ret += "Player " + str(player.get_player_num()) + ": " + player.get_name() + "\n"
-        return ret + "====\n"
 
+        ret = bcolors.OKCYAN + "Round " + str(self._round) +":" +bcolors.ENDC + "\n"
+        for id in self._active_players_ids:
+            self._players[id].prep_to_next_round()
+            ret += "Player " + str(self._players[id].get_player_num()) + ": " + self._players[id].get_name() + "\n"
+        return ret + bcolors.OKCYAN + "=======" + bcolors.ENDC
     
     def choose_question(self):
         '''The function chooses and returns this round statement as a string formatted: 
@@ -117,31 +129,69 @@ class GameManager:
             "
         '''
         random.shuffle(self._statements)
+
         self._this_round_statement = self._statements.pop()
-        return "True or False: " + self._this_round_statement[0]
+        return bcolors.UNDERLINE + "True or False:" + bcolors.ENDC + " " + bcolors.BOLD + self._this_round_statement[0] + bcolors.ENDC + '\n'
     
     def gather_answer(self, id, answer):
-        '''The function gathers an answer from each player, and verifies it. returns True if player was right otherwise False'''
-        if answer == self._this_round_statement[1]:
-            self._players[id].correct_answer()
-            return True
-        self._players[id].disqualify()
-        return False
+        '''The function gathers an answer from each player, and verifies it.'''
+        if id in self._active_players_ids:
+            if answer == self._this_round_statement[1]:
+                self._players[id].correct_answer()
+                return
+            self._players[id].disqualify()
 
     def sum_round(self):
         '''The function readies the game for the next round.'''
-        #gather active players
-        active_players = filter(lambda player: player.is_active(), self._players.values())
 
-        #disqualifying those who did not answer
-        map(lambda not_answered_player: not_answered_player.disqualify(), filter(lambda active_player: not active_player.has_answered_this_round(), active_players))
-
-        #if all the players answered wrong or did not answer, move on. else deactivate those who were wrong or did not answer in the given time.
-        if all(map(lambda active_players: active_players.is_disqualified(), active_players)):
-            map(lambda active_player: active_player.undisqualify(), active_players)
-        else:
-            map(lambda player: player.deactivate() ,filter(lambda active_player: active_player.is_disqualified(), active_players))
-            
-        #prepare next round
-        map(lambda player: player.prep_to_next_round(), self._players.values())    
         self._round += 1
+
+        #if all the questions are done
+        if (self._round > self._total_rounds):
+            remaining_players = list(map(lambda active_player: active_player.get_name(), filter(lambda player: player.get_id() in self._active_players_ids, self._players.values())))
+            self._game_active = False
+            return bcolors.OKGREEN + f"No more questions!\n {remaining_players} are the winners!" + bcolors.ENDC + "\n"
+        else:
+            #disqualifying those who did not answer this round
+            for id in self._active_players_ids:
+                if not self._players[id].has_answered_this_round():
+                    self._players[id].disqualify()
+
+            #check if all disqualified
+            move_on_round_flag = True
+            for id in self._active_players_ids:
+                if not self._players[id].is_disqualified():
+                    move_on_round_flag = False
+                    break
+
+            #if all the active players is disqualified this round, undisqualify them and move on the next round as is
+            if move_on_round_flag:
+                for id in self._active_players_ids:
+                    self._players[id].undisqualify()
+                return bcolors.FAIL + "All the players didn't know the answer. Moving on to the next round!" + bcolors.ENDC + "\n"
+            
+            # else deactivate the disqualified players
+            else:
+                ret = ""
+                disqualified_this_round = []
+                for id in self._active_players_ids:
+                    if self._players[id].is_disqualified():
+                        disqualified_this_round.append(id)
+                        ret += bcolors.FAIL + self._players[id].get_name() + " is incorrect!" + bcolors.ENDC + "\n"
+                    else:
+                        ret += bcolors.OKGREEN + self._players[id].get_name() + " is correct!"+ bcolors.ENDC + "\n"
+                
+                if len(disqualified_this_round) != len(self._active_players_ids):
+                    for id in disqualified_this_round:
+                        self.deactivate_player(id)
+
+                if len(self._active_players_ids) == 1:
+                    ret += bcolors.UNDERLINE + bcolors.OKCYAN + self._players[self._active_players_ids[0]].get_name() + " Wins!" +bcolors.ENDC + "\n"
+                    self._game_active = False   
+                return ret + '\n'
+
+    def sum_game(self):
+        '''For bonus purposes, returning list of tuples (player_name, player_score) of this game'''
+        return list(map(lambda player: (player.get_name(), player.get_score()), self._players.values()))
+        
+        
--- server.py
+++ server.py
@@ -3,77 +3,208 @@ import time
 import ipaddress
 import threading
 import struct
+import queue
+import playerconnection
+import gamemanager
+from logger import Logger
+
 
 server_name = 'PokemonTriviaKingByRonKaAndGalHa'
-port = 13117
+broadcast_port = 13117
 
-def log(src, msg):
-    print(time.strftime('[%d/%m/%Y - %H:%M:%S]') + ' ' + src + " :: " + msg) 
+#time to broadcast since last connection in seconds
+broadcast_time = 10
+sec = 1
+
+#wait for name to be sent after connection
+wait_for_name = 0.1
+
+#time of round in seconds
+round_time = 10
+
 
 class Server:
     def __init__(self):
         #initializing fields
+        threading.current_thread().setName("SERVER - MAIN THREAD")
         self._connections = dict()
-        self._state = "waiting_for_clients"
-        self._host = socket.gethostbyname(socket.gethostname())
+        self._msg_q = queue.Queue()
+        self._waiting_for_clients = True
+        self._id_distributor = 0
         
         #initializing the TCP socket
         self._tcp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self._tcp_sock.bind((self._host, 0))
+        #listen to all network interfaces
+        self._tcp_sock.bind(('', 0))
         self._port = self._tcp_sock.getsockname()[1]
         self._tcp_sock.listen()
-        log("SERVER", "Starting and now is ready for incoming connections on: " + str(self._tcp_sock.getsockname()))
+
+        #bonus field - leaderboard
+
+        self._leaderboard = list()
+        for i in range(10):
+            self._leaderboard.append(('NONE', 0))
+        
+        Logger.log(threading.current_thread().getName(), "Starting and now is ready for incoming connections...")
     
+    def notify_all(self):
+        for conn in self._connections.values():
+            if conn.is_active():
+                conn.notify()
+
+    def send(self, msg, cid):
+        self._connections[cid].send(msg, False)
+    
+    def send_all(self, msg):
+        for conn in self._connections.values():
+            if conn.is_active():
+                conn.send(msg, True)
+        Logger.log(threading.current_thread().getName(), "Sending message to all active connections: " + msg)
+
     def broadcast_offers(self):
         '''The function broadcasts offer messages to join the server'''
-
         #initializing the UDP broadcast socket       
         broadcaster = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        broadcaster.setsockopt(socket.SOL_SOCKET,socket.SO_BROADCAST,1)
-        broadcast_address = str(ipaddress.ip_network(self._host).broadcast_address)
-        magic_cookie = 0xabcddcba
-        offer = 0x2
+        broadcaster.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        broadcaster.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
         #broadcast message formatting
         pack_format = '>Ih32sH'
         to_broadcast = struct.pack(pack_format, 0xabcddcba, 0x2, server_name.encode(), self._port)
 
         #broadcast loop - the loop works until the server state changes
-        while self._state == "waiting_for_clients":
-            broadcaster.sendto(to_broadcast,(broadcast_address,port))
-            log("SERVER", "Broadcasting on UDP: " + str(to_broadcast))
+        while self._waiting_for_clients:
+            #broadcaster.sendto(to_broadcast,(broadcast_address,broadcast_port))
+            broadcaster.sendto(to_broadcast,('<broadcast>',broadcast_port))
+            Logger.log(threading.current_thread().getName(), "Broadcasting on UDP: " + str(to_broadcast))
             time.sleep(1)
+        
+        broadcaster.close()
 
         
     def run(self):
-        '''The function runs the server to work, broadcast offers and accept incoming connection. after 10 seconds without any new incoming connection, moving onto the game mode'''
+        '''The function runs the server, broadcast offers and accept incoming connection. after 10 seconds without any new incoming connection, moving onto the game mode'''
+        while(True):
+            self.waiting_for_clients()
+            self.game_mode()
+            self.reset()
 
+        
+    def waiting_for_clients(self):
         #initializing the broadcast thread
-        broadcast_thread = threading.Thread(group=None, target=self.broadcast_offers, name=None, args=(), kwargs={}, daemon=None)
+        broadcast_thread = threading.Thread(target=self.broadcast_offers, name = "SERVER - BROADCASTING THREAD", daemon=True)
         broadcast_thread.start()
 
         #handle incoming connections, timing out the while loop after 10 seconds without new connection     
-        while self._state == "waiting_for_clients":
-            self._tcp_sock.settimeout(10)
+        while True:            
             try:
-                conn = self._tcp_sock.accept()               
+                conn, addr = self._tcp_sock.accept()               
             except socket.timeout:
                 break
-            self._connections[len(self._connections)] = conn
             
-            log("SERVER", "New client connected: " + str(conn[1]))
-            print(str(conn[0].recv(1)))
+            #creating a threaded blocking connection handler for each client if the name is valid, else reject the socket connection
+            try:
+                conn.setblocking(True)
+                conn.settimeout(wait_for_name)         
+                Logger.log(threading.current_thread().getName(), "New client connected on: " + str(addr) + "     ID: [" + str(self._id_distributor) + "]")
+                Logger.log(threading.current_thread().getName(), "Authenticating...")
+                #here is waiting for the predefined and sent name of the client 0.1sec since ping in the LAN is around 3ms so 0.1 is more than enough to wait
+                name = conn.recv(1024).decode()
+                if playerconnection.PlayerConnection.validate_name(name):
+                    self._connections[self._id_distributor] = playerconnection.PlayerConnection(conn, addr, self._id_distributor, self._msg_q, name[0:-1])
+                    Logger.log(threading.current_thread().getName() , "Client ID [" + str(self._id_distributor) + "] has connected succesfuly!" )
+                    self._id_distributor+=1
+                    #waiting for another 10 seconds since last sucessful connection 
+                    self._tcp_sock.settimeout(broadcast_time)
+                else:
+                    #name not valid -> close the connection
+                    Logger.log(threading.current_thread().getName(), "Client on: " + str(addr) + " has sent illegal name (Format 2-25 chars long ending with backslash n), closing the connection!")
+                    conn.close()
+            except BlockingIOError:
+                #name did not sent -> close the connection
+                Logger.log(threading.current_thread().getName(), "Client on: " + str(addr) + " did not send his name immediately, closing the connection!")
+                conn.close()
         
-        #no new connections in the last 10 seconds, starting game mode
-        self._state = "game_mode"
-        self.game_mode()
         
+        self._waiting_for_clients = False
+        broadcast_thread.join()
+            
+            
+
 
     def game_mode(self):
-        '''The function running the game itself, answering Trivia questions, collecting answers from the clients, and managing the game'''
+        '''The function running the game itself, sending Trivia questions to the players, collecting answers from the clients, and managing the game'''
+        self.send_all("Welcome to " + server_name + " server, where we answer trivia questions about Pokemon!\n\n")
+        #first creating a GameManager obj and adding all the clients as players to it
+        gm = gamemanager.GameManager()
+        for conn in self._connections.values():
+            if conn.is_active():
+                gm.add_player(conn.get_id(), conn.get_name())
 
-        print("Welcome to " + server_name + " server, where we are answering Trivia questions about Pokemon!!")
-        ##TODO implement game mode
+        #game loop, gathering answers until timout, checking the correctness and sending messages to clients
+        while gm.game_active() and len(list(filter(lambda conn: conn.is_active(), self._connections.values())))>0:
+            self.send_all(gm.round_init())
+            self.send_all(gm.choose_question())
+            time.sleep(round_time - sec)
+            #waking up the socket threads - to put their ans in the msg_q 
+            self.notify_all()
+            time.sleep(sec)
+            while True:
+                try:
+                    msg = self._msg_q.get(block = False)
+                    id = msg[0]
+                    ans = msg[1]
+                    if ans in ['y', 'Y', '1', 'T']:
+                        ans = True  
+                    elif ans in ['n', 'N', '0', 'F']:
+                        ans = False
+                    else:
+                        Logger.log("GM" , "Received illegal answer from the player ID [" + str(id) + "]: " + str(ans))
+                        continue
+                    gm.gather_answer(id, ans)
+                except queue.Empty:
+                    break
+                
+            self.send_all(gm.sum_round())
+        
+        #exiting the while loop means the game has ended
+
+        #bonus purpose, leaderboards update
+        for name, score in gm.sum_game():
+            for i in range(0,10):
+                if score > self._leaderboard[i][1]:
+                    self._leaderboard.insert(i, (name, score))
+                    self._leaderboard.pop()
+                    break
+        
+        self.send_all(self.leader_board_string())
+            
+                
+    def leader_board_string(self):
+        leader_board_string = 'LEADERBOARD      NAME                     SCORE\n===========      ====                     =====\n\n'
+        count = 1
+        for name,score in self._leaderboard:
+            pad2 = " "*(25-len(name))
+            pad1 = " "*15
+            if count == 10:
+                pad1 = " "*14
+            leader_board_string += str(count) + "." + pad1 + name + pad2 + str(score) + "\n"
+            count+=1
+        return leader_board_string
+
+         
+    def reset(self):
+        '''The functions resets the server: closing the open connections and readies the server for another round.'''
+        for conn in self._connections.values():
+            conn.close()
+        Logger.log(threading.current_thread().getName(), "Game over, sending out offer requests...")
+        self._connections = dict()
+        self._msg_q = queue.Queue()
+        self._id_distributor = 0
+        self._waiting_for_clients = True
+        self._tcp_sock.settimeout(None)
+        time.sleep(sec)
+        
 
 s = Server()
 s.run()
